c206a94ae7f436be8a230dcdcb50ad225318ebf8
Praveen00013 <praveenpraveen0013@gmail.com>
2024-12-26 12:09:17 +0530

    new coponent created

---
+++ src/app/test-app/test-app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-app',
+  imports: [],
+  templateUrl: './test-app.component.html',
+  styleUrl: './test-app.component.css'
+})
+export class TestAppComponent {
+
+}
